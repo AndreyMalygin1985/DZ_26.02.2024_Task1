436139b1259689239a7c3802c28ec238afe8ac34
Андрей <dusha22075@gmail.com>
2024-02-23 13:28:45 +0300

    +

--- DZ_26.02.2024_Task1/Program.cs
+++ DZ_26.02.2024_Task1/Program.cs
@@ -9,7 +9,6 @@ class Program
 {
     static void Main()
     {
-        // Объявление одномерного массива A и заполнение его пользовательским вводом
         double[] A = new double[5];
         Console.WriteLine("Введите 5 чисел для массива A:");
         for (int i = 0; i < A.Length; i++)
@@ -17,18 +16,16 @@ class Program
             A[i] = Convert.ToDouble(Console.ReadLine());
         }
 
-        // Объявление двумерного массива B и заполнение его случайными числами
         double[,] B = new double[3, 4];
         Random rand = new Random();
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 4; j++)
             {
-                B[i, j] = rand.NextDouble() * 100; // Генерация случайных чисел от 0 до 100
+                B[i, j] = rand.NextDouble() * 100;
             }
         }
 
-        // Вывод массива A в одну строку
         Console.Write("Массив A: ");
         foreach (var num in A)
         {
@@ -36,7 +33,6 @@ class Program
         }
         Console.WriteLine();
 
-        // Вывод массива B в виде матрицы
         Console.WriteLine("\nМассив B:");
         for (int i = 0; i < 3; i++)
         {
@@ -47,24 +43,19 @@ class Program
             Console.WriteLine();
         }
 
-        // Нахождение общего максимального и минимального элементов
         double maxA = A.Max();
         double minA = A.Min();
         double maxB = B.Cast<double>().Max();
         double minB = B.Cast<double>().Min();
 
-        // Нахождение общей суммы всех элементов
         double sumA = A.Sum();
         double sumB = B.Cast<double>().Sum();
 
-        // Нахождение общего произведения всех элементов
         double prodA = A.Aggregate((x, y) => x * y);
         double prodB = B.Cast<double>().Aggregate((x, y) => x * y);
 
-        // Нахождение суммы четных элементов массива A
         double sumEvenA = A.Where(num => num % 2 == 0).Sum();
 
-        // Нахождение суммы нечетных столбцов массива B
         double sumOddColumnsB = 0;
         for (int j = 0; j < 4; j++)
         {
@@ -73,13 +64,12 @@ class Program
             {
                 columnSum += B[i, j];
             }
-            if (j % 2 != 0) // Нечетный столбец
+            if (j % 2 != 0)
             {
                 sumOddColumnsB += columnSum;
             }
         }
 
-        // Вывод результатов
         Console.WriteLine("\nОбщий максимальный элемент:");
         Console.WriteLine("Массив A: " + maxA);
         Console.WriteLine("Массив B: " + maxB);
